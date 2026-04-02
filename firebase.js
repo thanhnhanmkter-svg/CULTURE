8675c19c4b91ee07b71d782c5f2f3a9e3103a93c
thanhnhanmkter-svg <thanhnhanmkter@gmail.com>
2026-04-02 23:50:02 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -17,17 +17,16 @@ import {
   serverTimestamp,
 } from 'firebase/database'
 
-const firebaseConfig = {https://workshop-culture-default-rtdb.firebaseio.com/}
-  apiKey:            'AIzaSyDRoQRCV7fr-1yN071g8Vdi2u1sOvEtHBA',
-  authDomain:        'workshop-culture.firebaseapp.com',
-  projectId:         'workshop-culture',
-  storageBucket:     'workshop-culture.firebasestorage.app',
-  messagingSenderId: '741916433991',
-  appId:             '1:741916433991:web:b401d078c3446d591f9b62',
-  measurementId:     'G-9QSHBP1GJV',
-  // ⚠️ QUAN TRỌNG: Thêm databaseURL cho Realtime Database
-  databaseURL:       'https://workshop-culture-default-rtdb.asia-southeast1.firebasedatabase.app',
-}
+const firebaseConfig = {
+  databaseURL: "https://workshop-culture-default-rtdb.firebaseio.com/",
+  apiKey: "AIzaSyDRoQRCV7fr-1yN071g8Vdi2u1sOvEtHBA",
+  authDomain: "workshop-culture.firebaseapp.com",
+  projectId: "workshop-culture",
+  storageBucket: "workshop-culture.appspot.com",
+  messagingSenderId: "741916433991",
+  appId: "1:741916433991:web:b401d078c3446d591f9b62"
+};
+
 
 // Singleton — tránh khởi tạo nhiều lần trong Next.js HMR
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
